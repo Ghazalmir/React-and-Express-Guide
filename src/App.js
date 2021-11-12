d9cac55a06e9c130cd3cfca409e421794e4ad820
Wahaj <wahajhaider32@gmail.com>
2021-11-12 14:43:39 -0500

    created: page4 component (weather-app.js), back-end folder (as a seperate node application), basic express server, proxy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import WeatherApp from './pages/page04/weather-app';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
-      <h2>Page 4:</h2>
-      <WeatherApp></WeatherApp>
     </div>
   );
 }
