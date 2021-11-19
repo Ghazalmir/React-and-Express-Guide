e6e7a1f08d929db940c1297f807d3601e9c9a977
Ghazal Mir <ghazalmirsayah@gmail.com>
2021-11-18 20:29:27 -0500

    Add footer

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 //import Tutorial from './pages/page03/tutorial';
 import Btn from './components/button/button'
 import CustomNavbar from "./components/navbar/navbar";
-
+import Footer from './components/footer/footer';
 // can comment pages out as they are just there for testing purposes
 function App() {
   return (
     <div className="App">
       <CustomNavbar />
 
+      <Footer />
     </div>
   );
 }
