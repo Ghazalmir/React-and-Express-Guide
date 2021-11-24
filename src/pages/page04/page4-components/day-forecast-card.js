11075fdb7a4ed23cb3af0b9e73e995782fa01b49
Wahaj <wahajhaider32@gmail.com>
2021-11-23 23:26:42 -0500

    finished main functionalities of page 4

--- src/pages/page04/page4-components/day-forecast-card.js
+++ src/pages/page04/page4-components/day-forecast-card.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./day-forecast-card-styles.module.css";
+import axios from "axios";
 
-const DayForecastCard = ({day, temp}) => {
+const DayForecastCard = ({ day, temp, icon }) => {
+	// useEffect(() => {
+	// 	updateInfo();
+	// });
+
+	let iconUrl = `https://www.weatherbit.io/static/img/icons/${icon}.png`;
+
 	return (
 		<div className={styles.dayForecastCard}>
 			<div className={styles.header}>
 				<h3>{day}</h3>
+			</div>
+			<div className={styles.icon}>
+				<img
+					src={iconUrl}
+					alt=""
+				/>
 			</div>
 			<div className={styles.info}>
 				<h3>{temp}</h3>
