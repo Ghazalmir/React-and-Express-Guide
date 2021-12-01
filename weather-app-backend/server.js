b83901427ac916e6e6196daa746d83e434642b47
Wahaj <wahajhaider32@gmail.com>
2021-12-01 00:05:47 -0500

    deployed backend server using heroku

--- weather-app-backend/server.js
+++ weather-app-backend/server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
 	res.send("Express here!");
 });
+
+app.get("/favicon.ico", (req, res) => res.status(204));
 
 app.post("/weather-info", async (req, res) => {
 	let latitude = req.body.latitude;
