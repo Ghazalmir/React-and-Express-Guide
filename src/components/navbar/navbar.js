fab72bdf40c3d380915d6a06f8247e82a24c7e86
Ghazal Mir <ghazalmirsayah@gmail.com>
2021-12-01 22:55:21 -0500

    Minor styling chabges

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -7,28 +7,24 @@ import { Link } from "react-router-dom";
 function CustomNavbar(){
   return(
 <div className={styles.nav}>
-    <input type="checkbox" id="navCheck" className={styles.navCheck}/>
-    <div className={styles.navHeader}>
+    <input type="checkbox" id="navCheck" className={styles.Check}/>
       <div className={styles.logo}>
         LOGO
       </div>
-    </div>
-    <div className={styles.navBtn}>
+
+    <div className={styles.Btn}>
       <label for="navCheck">
         <span>&#9776;</span>
       </label>
     </div>
     
-    <div className={styles.navLinks}>
-
-
+    <div className={styles.Links}>
       <Link to="/summary" className={styles.Link}>Summary</Link>
       <Link to="/installation" className={styles.Link}>Installation</Link>
       <Link to="/tutorial" className={styles.Link}>Tutorial</Link>
       <Link to="/demo" className={styles.Link}>Sample Page</Link>
       <Link to="/conslusion" className={styles.Link}>Conclusion</Link>
       <Link to="/credits" className={`${styles.Link} ${styles.lastLink}`}>Credits</Link>
-
     </div>
   </div>
   );
